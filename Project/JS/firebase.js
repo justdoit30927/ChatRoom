10eaba90407e2d691b7bb0cf2d0447669c11fc3a
Huang Sheng Fu <justdoit30927@gmail.com>
2019-09-16 00:40:32 +0800

    聊天室完成出版

---
+++ Project/JS/firebase.js
@@ -0,0 +1,15 @@
+/*設定*/
+var firebaseConfig = {
+    apiKey: "AIzaSyB5X2WXQwY56bGMCnyg81Vda2ADCSCgDRk",
+    authDomain: "chatroom-2f479.firebaseapp.com",
+    databaseURL: "https://chatroom-2f479.firebaseio.com",
+    projectId: "chatroom-2f479",
+    storageBucket: "",
+    messagingSenderId: "759454282290",
+    appId: "1:759454282290:web:ce9bab25269f658e"
+};
+firebase.initializeApp(firebaseConfig);
+
+
+
+
